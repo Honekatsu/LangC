8dd926dde9e460f8c9024909cdb8887aba2f3123
katsu <e3531901@eng.kitakyu-u.ac.jp>
2023-11-17 14:30:27 +0900

    main2 to kadai

--- main2.c
+++ main2.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int main(void){
+    /* 課題1 */
+    printf("Welcome to C Programming!\n");
+
+    /* 課題2 */
+    int x = 10;
+    int y = 2*x;
+    int z = x+y;
+
+    printf("x=%d y=%d z= %d\n",x,y,z);
+
+    /* 課題3 */
     int price;
-    price = 10;
-    printf("%d",price);
+    price = 500;
+    printf("%f\n",500*1.1);
     return 0;
 }
